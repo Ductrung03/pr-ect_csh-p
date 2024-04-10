9517bf0fc11dfefdb48700e0912fd1ef5e70aa28
LuckyBoi <133234403+Ductrung03@users.noreply.github.com>
2024-04-10 14:31:31 +0700

    ductrung

---
+++ Dotnet6MvcLogin/Controllers/QSController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcLogin.Data;
+using MvcLogin.Models;
+
+namespace MvcLogin.Controllers
+{
+    public class QSController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<QuanSo> quanSos = new List<QuanSo>();
+
+            QuanLyQuanSo quanLyQuanSo = new QuanLyQuanSo();
+
+            quanSos = quanLyQuanSo.GetBaoCaoQuanSo();
+            return View(quanSos);
+        }
+
+        public ActionResult AddQuanSo()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewQuanSo(QuanSo qs)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    QuanLyQuanSo _DbQs = new QuanLyQuanSo();
+                    if (_DbQs.AddQuanSo(qs))
+                    {
+                        return RedirectToAction("AddQuanSo");
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+       
+    }
+}
---
+++ Dotnet6MvcLogin/Models/QuanSo.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcLogin.Models
+{
+    public class QuanSo
+    {
+        public int IdBcqs { get; set; }
+
+        public string? IdDv { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? Ngay { get; set; }
+
+        public int? TongQs { get; set; }
+
+        public int? QsVang { get; set; }
+
+        public int? DaoNgu { get; set; }
+
+        public int? DiVien { get; set; }
+
+        public int? BenhXa { get; set; }
+
+        public int? DiHoc { get; set; }
+
+        public int? DiThucTe { get; set; }
+
+        public int? DiThucTap { get; set; }
+
+        public int? DiTt { get; set; }
+
+        public int? DiCtac { get; set; }
+
+        public int? ThaiSan { get; set; }
+
+        public int? LyDoKhac { get; set; }
+
+        public string? ChuThich { get; set; }
+
+        //public virtual DonVi? IdDvNavigation { get; set; }
+    }
+}
